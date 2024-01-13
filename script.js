87ceeb1d2261ef8f1a73445a127cd16c9e98c0ac
newelhan <ne.welhan@gmail.com>
2024-01-13 09:53:19 +0700

    Finished the main features:
    - Fixed the conditions for the grid size prompt
    - Made an interactable button to change the grid size
    - Created an input on the top right of the website to change the grid color when hovering
    
    May fix some features soon

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const mainGrid = document.querySelector('.main-grid');
 
+document.querySelector('.change-grid-button')
+.addEventListener('click', () => {
+  generateGrid(setGridSize());
+})
+
 function setGridSize() {
   while (true) {
     let gridSize = prompt('Enter the grid amount (10-100)', '16')
-      if (isNaN(gridSize) || !gridSize) {
-        alert('Enter the proper amount')
-      } else if (gridSize > 100) {
-        alert('Grid size must be less than 100 spaces')
-      } else if (gridSize < 10) {
-        alert('Grid size must be more than 10 spaces')
-      } else {
+    //checks each case, whether it's true or not
+    //compares the line with the expression (true in this case)
+    //expression should mostly be a boolean, as you will compare each line whether it's true or false
+    switch (true) {
+      case isNaN(gridSize) || !gridSize: 
+        alert('Grid size entered is invalid, grid is automatically 16x16')
+        return gridSize = 16;
+
+      case gridSize > 100:
+        alert('Grid size must be less 100x100');
+        break;
+      
+      case gridSize < 10:
+        alert('Grid size must be more than 10x10');
+        break;
+
+      default:
         return gridSize;
-      }
+    }
   }
 }
-
 
 function generateGrid(amount) {
   let gridColumns = ``;
@@ -31,17 +45,18 @@ function generateGrid(amount) {
 
   mainGrid.innerHTML = gridColumns;
 
+  let gridColor;
+  const inputColor =  document.querySelector('.input-color');
+  inputColor.oninput = () => {
+    gridColor = inputColor.value;
+  };
+
   //grid hover
   document.querySelectorAll('.grid-column div').forEach((element) => {
     element.addEventListener('mouseover', () => {
-      element.style.backgroundColor = 'black';
+      element.style.backgroundColor = gridColor || 'black';
     });
   });
 };
-
-document.querySelector('.js-change-grid')
-  .addEventListener('onclick', () => {
-    generateGrid(setGridSize());
-  })
 
 generateGrid(setGridSize());
